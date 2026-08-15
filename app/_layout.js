fbd5c58771e51ab76c459ac0fc7029c92cd9bb89
Dan <dan.hornblower@gmail.com.com>
2026-08-15 14:38:10 +0100

    Add Sentry error monitoring

--- app/_layout.js
+++ app/_layout.js
@@ -6,8 +6,28 @@ import { View, ActivityIndicator } from 'react-native';
 import { WatchlistProvider } from '../lib/WatchlistContext';
 import { GamesProvider } from '../lib/GamesContext';
 import { colors } from '../lib/theme';
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://9d423463abf5423c4015c039ac00dce8@o4511915073601536.ingest.de.sentry.io/4511915081990224',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded] = useFonts({
     Poppins_600SemiBold,
     Poppins_700Bold,
@@ -42,4 +62,4 @@ export default function RootLayout() {
       </WatchlistProvider>
     </GamesProvider>
   );
-}
+});
